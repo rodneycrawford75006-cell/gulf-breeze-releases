826397bfedf353f2535e30e11a783f5b44127744
rodneycrawford75006-cell <rodneycrawford75006@gmail.com>
2026-08-14 20:20:31 +0000

    Use WordPress globals in route resolver tests

--- validation/core-access-test.php
+++ validation/core-access-test.php
@@ -86,19 +86,19 @@ $core = Gulf_Breeze_Configuration::instance();
 $resolver = new ReflectionMethod( $core, 'current_learnpress_item_id' );
 $resolver->setAccessible( true );
 $original_request_uri = $_SERVER['REQUEST_URI'] ?? '';
-$wp_query = new WP_Query( array( 'post_type' => 'lp_course', 'p' => $course_id ) );
-$post = get_post( $course_id );
-setup_postdata( $post );
+$GLOBALS['wp_query'] = new WP_Query( array( 'post_type' => 'lp_course', 'p' => $course_id ) );
+$GLOBALS['post'] = get_post( $course_id );
+setup_postdata( $GLOBALS['post'] );
 $_SERVER['REQUEST_URI'] = '/courses/controlled-access-validation-course/lessons/validation-locked-lesson-two/';
 if ( $lesson_two_id !== $resolver->invoke( $core ) ) {
 	throw new RuntimeException( 'Nested LearnPress route resolver did not identify lesson two.' );
 }
 
-$wp_query = new WP_Query();
-$wp_query->queried_object = get_post( $lesson_one_id );
-$wp_query->queried_object_id = $lesson_one_id;
-$post = get_post( $lesson_one_id );
-setup_postdata( $post );
+$GLOBALS['wp_query'] = new WP_Query();
+$GLOBALS['wp_query']->queried_object = get_post( $lesson_one_id );
+$GLOBALS['wp_query']->queried_object_id = $lesson_one_id;
+$GLOBALS['post'] = get_post( $lesson_one_id );
+setup_postdata( $GLOBALS['post'] );
 $_SERVER['REQUEST_URI'] = '/?post_type=lp_lesson&p=' . $lesson_one_id;
 if ( $lesson_one_id !== $resolver->invoke( $core ) ) {
 	throw new RuntimeException( 'Ordinary lesson query resolver did not identify lesson one.' );
